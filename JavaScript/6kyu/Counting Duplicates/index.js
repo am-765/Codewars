13f1210db98c98ba07054dd1f11350ea0edd9e60
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2020-11-07 11:22:35 +0900

    Counting Duplicates コンソールログ削除

--- JavaScript/6kyu/Counting Duplicates/index.js
+++ JavaScript/6kyu/Counting Duplicates/index.js
@@ -6,10 +6,3 @@ function duplicateCount(text) {
     return textArr.filter((cv, i) => textArr.indexOf(cv) === i && textArr.lastIndexOf(cv) !== i).length;
   }
 }
-
-console.log(duplicateCount(""));
-console.log(duplicateCount("abcde"));
-console.log(duplicateCount("aabbbcde"));
-console.log(duplicateCount("aabBcde"));
-console.log(duplicateCount("Indivisibility"));
-console.log(duplicateCount("Indivisibilities"));
