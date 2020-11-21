d2094fbbb780870035d83a28a4478316f39ee6ce
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2020-11-22 03:05:04 +0900

    Take a Ten Minute Walk console.log削除

--- JavaScript/6kyu/Take a Ten Minute Walk/index.js
+++ JavaScript/6kyu/Take a Ten Minute Walk/index.js
@@ -9,8 +9,3 @@ function isValidWalk(walk) {
     return false
   }
 }
-
-console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']))
-console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']))
-console.log(isValidWalk(['w']))
-console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']))
