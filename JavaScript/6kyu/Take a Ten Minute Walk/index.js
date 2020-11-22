a2c268b2d55aa54e167be1e109b062ef08d95b70
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2020-11-22 15:12:41 +0900

    Take a Ten Minute Walk 簡潔な記述に変更

--- JavaScript/6kyu/Take a Ten Minute Walk/index.js
+++ JavaScript/6kyu/Take a Ten Minute Walk/index.js
@@ -1,11 +1,4 @@
 function isValidWalk(walk) {
-  if (walk.length === 10) {
-    const n = walk.join('').match(/[n]/gi).length
-    const s = walk.join('').match(/[s]/gi).length
-    const e = (walk.join('').match(/[e]/gi) || []).length
-    const w = (walk.join('').match(/[w]/gi) || []).length
-    return n === s && e === w ? true : false
-  } else {
-    return false
-  }
+  const getCount = (val) => walk.filter(el => el === val).length
+  return walk.length === 10 && getCount('n') === getCount('s') && getCount('e') === getCount('w')
 }
