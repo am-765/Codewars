cf07d315b17f9a00ebfaef7a70eb2d10664e4773
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-01-07 15:37:49 +0900

    Create Phone Number 簡潔な記述に変更

--- JavaScript/6kyu/Create Phone Number/index.js
+++ JavaScript/6kyu/Create Phone Number/index.js
@@ -1,3 +1,6 @@
-function createPhoneNumber(numbers){
-  return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/g, '($1) $2-$3')
+function createPhoneNumber(numbers) {
+  function getNumber(start, end) {
+    return numbers.join('').substring(start, end)
+  }
+  return `(${getNumber(0, 3)}) ${getNumber(3, 6)}-${getNumber(6, 10)}`
 }
