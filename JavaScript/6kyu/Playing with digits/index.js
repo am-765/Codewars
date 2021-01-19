b54f3c0d60959927e69a870ce00689c6804090bd
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-01-19 11:29:32 +0900

    簡潔な記述に変更

--- JavaScript/6kyu/Playing with digits/index.js
+++ JavaScript/6kyu/Playing with digits/index.js
@@ -1,4 +1,4 @@
 function digPow(n, p) {
-  const calc = n.toString().split('').map((cv, id)=> cv ** (p + id)).reduce((ac, cv) => ac + cv)
-  return calc % n ? -1 : calc / n
+  const calcResult = n.toString().split('').reduce((ac, cv, id) => ac + (Math.pow(cv, p + id)), 0);
+  return calcResult % n ? -1 : calcResult / n
 }
