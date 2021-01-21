d2a0f5c974cfed502c64945bdc8b317f3a8224d0
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-01-22 01:12:44 +0900

    簡潔な記述に変更

--- JavaScript/4kyu/Snail/index.js
+++ JavaScript/4kyu/Snail/index.js
@@ -1,41 +1,9 @@
 snail = function(array) {
-  let x = array.length,
-    y = array.length,
-    result = [];
-
-  if (y === 1) {
-    return array[0]
+  let result = [];
+  while (array.length) {
+    result.push(...array.shift());
+    array.map(cv => result.push(cv.pop()));
+    array.reverse().map(cv => cv.reverse());
   }
-
-  while (y > 1) {
-    for (let i = 0; i < x; i++) {
-      result.push(array[0][i]);
-    }
-    array.splice(0, 1);
-    y--;
-
-    for (let j = 0; j < y; j++) {
-      result.push(array[j][y]);
-      array[j].splice(y);
-    }
-    x--;
-
-    for (let k = x - 1; 0 <= k; k--) {
-      result.push(array[x - 1][k]);
-    }
-    array.splice(x - 1);
-    y--;
-
-    for (let l = y - 1; 0 <= l; l--) {
-      result.push(array[l][0]);
-      array[l].splice(0, 1);
-    }
-    x--;
-
-    if (x === 1 && y === 1) {
-      result.push(array[0][0])
-    }
-  }
-
   return result
 }
