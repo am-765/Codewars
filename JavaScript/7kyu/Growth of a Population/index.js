725c1a8078a04552d6a49021c46bbb407e492252
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-02-02 10:38:59 +0900

    Growth of a Population 簡潔な記述に変更

--- JavaScript/7kyu/Growth of a Population/index.js
+++ JavaScript/7kyu/Growth of a Population/index.js
@@ -1,8 +1,7 @@
 function nbYear(p0, percent, aug, p) {
-  let count = 0;
-  while (p0 < p) {
-    p0 = p0 + p0 * (percent / 100) + aug;
-    count++;
+  let i = 0;
+  for (i; p0 < p; i++) {
+    p0 = p0 * (1 + percent / 100) + aug;
   }
-  return count;
+  return i;
 }
