c8830a782d8734817c1fc732338be6a2fc04cb6f
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-02-02 10:22:22 +0900

    Sum of odd numbers 簡潔な記述に変更

--- JavaScript/7kyu/Sum of odd numbers/index.js
+++ JavaScript/7kyu/Sum of odd numbers/index.js
@@ -1,11 +1,1 @@
-function rowSumOddNumbers(n) {
-  let x = 1;
-  let y = [];
-  for (let i = 0; i < n; i++) {
-    x += 2 * i;
-  }
-  for (let j = 0; j < n; j++) {
-    y.push(x + j * 2);
-  }
-  return y.reduce((ac, cv) => ac + cv, 0);
-}
+const rowSumOddNumbers = n => Math.pow(n, 3);
